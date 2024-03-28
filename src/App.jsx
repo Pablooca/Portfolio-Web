27f3d424db7c14b583c09d297fc4ba2b34598d21
Pablo Oca Galeano <85012902+Pablooca@users.noreply.github.com>
2024-03-28 10:42:08 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pablo Oca Galeano",
+  title: "Junior Back End Developer",
+  email: "pabloocagal03@gmail.com",
+  gitHub: "Pablooca",
+  instagram: "Pablooca_03",
+  linkedIn: "pablo-oca",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
